82b8c4ef593b419d752c4cc77d6a0103ce989951
Pavel Savchenko <asfaltboy@gmail.com>
2019-08-23 11:39:37 +0200

    refactor: make use of TextCommand.view and cleanup
    TextCommand sets self.view according to documentation:
    https://www.sublimetext.com/docs/3/api_reference.html#sublime_plugin.TextCommand
    
    In addition, most of the code is superficial wrappers to a single call, so
    we can remove them to make code shorter and easier to read.

--- isort_file.py
+++ isort_file.py
@@ -10,52 +10,31 @@ sys.path.append(os.path.dirname(__file__))
 
 
 class IsortCommand(sublime_plugin.TextCommand):
-    view = None
+    def get_region(self):
+        return sublime.Region(0, self.view.size())
 
-    def get_region(self, view):
-        return sublime.Region(0, view.size())
-
-    def get_buffer_contents(self, view):
-        return view.substr(self.get_region(view))
-
-    def set_view(self):
-        self.view = sublime.active_window().active_view()
-        return self.view
-
-    def get_view(self):
-        if self.view is None:
-            return self.set_view()
-
-        return self.view
+    def get_buffer_contents(self):
+        return self.view.substr(self.get_region())
 
     def set_cursor_back(self, begin_positions):
-        this_view = self.get_view()
         for pos in begin_positions:
-            this_view.sel().add(pos)
+            self.view.sel().add(pos)
 
-    def get_positions(self):
-        pos = []
-        for region in self.get_view().sel():
-            pos.append(region)
-        return pos
+    def sort_imports(self, edit):
+        current_positions = list(self.view.sel())
 
-    def get_settings(self):
-        profile = sublime.active_window().active_view().settings().get('isort')
-        return profile or {}
-
-    def run(self, edit):
-        this_view = self.get_view()
-        current_positions = self.get_positions()
-
-        this_contents = self.get_buffer_contents(this_view)
-        settings = self.get_settings()
+        this_contents = self.get_buffer_contents()
+        settings = self.view.settings().get('isort') or {}
         sorted_imports = SortImports(
             file_contents=this_contents,
             **settings
         ).output
-        this_view.replace(edit, self.get_region(this_view), sorted_imports)
+        self.view.replace(edit, self.get_region(), sorted_imports)
 
         # Our sel has moved now..
-        remove_sel = this_view.sel()[0]
-        this_view.sel().subtract(remove_sel)
+        remove_sel = self.view.sel()[0]
+        self.view.sel().subtract(remove_sel)
         self.set_cursor_back(current_positions)
+
+    def run(self, edit):
+        self.sort_imports(edit)
