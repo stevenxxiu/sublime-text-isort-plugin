930451513186ec7e09dbfcb42545111ad6e4715a
Steven Xu <stevenxxiu@gmail.com>
2021-09-28 20:31:08 +1000

    feat: bumped to *isort 5.9.3*

--- isort/pylama_isort.py
+++ isort/pylama_isort.py
@@ -1,29 +1,45 @@
 import os
 import sys
+from contextlib import contextmanager
+from typing import Any, Dict, Iterator, List, Optional
 
-from pylama.lint import Linter as BaseLinter
+from pylama.lint import Linter as BaseLinter  # type: ignore
 
-from .isort import SortImports
+from isort.exceptions import FileSkipped
+
+from . import api
 
 
-class Linter(BaseLinter):
+@contextmanager
+def supress_stdout() -> Iterator[None]:
+    stdout = sys.stdout
+    with open(os.devnull, "w") as devnull:
+        sys.stdout = devnull
+        yield
+        sys.stdout = stdout
 
-    def allow(self, path):
+
+class Linter(BaseLinter):  # type: ignore
+    def allow(self, path: str) -> bool:
         """Determine if this path should be linted."""
-        return path.endswith('.py')
+        return path.endswith(".py")
 
-    def run(self, path, **meta):
+    def run(
+        self, path: str, params: Optional[Dict[str, Any]] = None, **meta: Any
+    ) -> List[Dict[str, Any]]:
         """Lint the file. Return an array of error dicts if appropriate."""
-        with open(os.devnull, 'w') as devnull:
-            # Suppress isort messages
-            sys.stdout = devnull
+        with supress_stdout():
+            try:
+                if not api.check_file(path, disregard_skip=False, **params or {}):
+                    return [
+                        {
+                            "lnum": 0,
+                            "col": 0,
+                            "text": "Incorrectly sorted imports.",
+                            "type": "ISORT",
+                        }
+                    ]
+            except FileSkipped:
+                pass
 
-            if SortImports(path, check=True).incorrectly_sorted:
-                return [{
-                    'lnum': 0,
-                    'col': 0,
-                    'text': 'Incorrectly sorted imports.',
-                    'type': 'ISORT'
-                }]
-            else:
-                return []
+            return []
